fdb337f72cd33c426732f353767c40ae866f06fe
ingrid-arce <160282857+ingrid-arce@users.noreply.github.com>
2024-06-24 11:45:17 -0500

    backend

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const { pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres.avbcuctiwucxbufqvpzu',
+    host: 'aws-0-us-west-1.pooler.supabase.com',
+    database: 'postgres',
+    password: 'vskMqqkVjQjqP21l',
+    port: '6543',
+});
+pool.on('error',(err, client)=>{
+    console.error('Error inesperado en el pool de conexiones:', err);
+    process.exit(-1);
+});
+module.exports = pool;
