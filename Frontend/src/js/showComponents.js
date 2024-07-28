55684127cbdfa34a3ddd52dd1dbd0fb4cc9ec8c6
LuisIbarguen <160256045+LuisIbarguen@users.noreply.github.com>
2024-07-28 13:13:11 -0500

    rutas de la dashboard

---
+++ Frontend/src/js/showComponents.js
@@ -0,0 +1,37 @@
+import {handleButtonClick} from "../components/Dashboard/sidebar.astro"
+
+export function showComponent(component) {
+    const registroP = document.getElementById('registroP');
+    const registro = document.getElementById('registro');
+    const registroAdmin = document.getElementById('registroAdmin');
+    const tablaCompras = document.getElementById('tablaCompras');
+    const tablaAdmin = document.getElementById('tablaAdmin');
+    const tablaProvi = document.getElementById('tablaProvi');
+    const perfil = document.getElementById('perfil');
+
+    if (registroP) registroP.style.display = 'none';
+    if (registro) registro.style.display = 'none';
+    if (registroAdmin) registroAdmin.style.display = 'none';
+    if (tablaCompras) tablaCompras.style.display = 'none';
+    if (tablaAdmin) tablaAdmin.style.display = 'none';
+    if (tablaProvi) tablaProvi.style.display = 'none';
+    if (perfil) perfil.style.display = 'none';
+
+    if (component === 'registroP' && registroP) {
+        registroP.style.display = 'block';
+    } else if (component === 'registro' && registro) {
+        registro.style.display = 'block';
+    } else if (component === 'tablaAdmin' && tablaAdmin) {
+        tablaAdmin.style.display = 'block';
+    } else if (component === 'tablaProvi' && tablaProvi) {
+        tablaProvi.style.display = 'block';
+    } else if (component === 'registroAdmin' && registroAdmin) {
+        registroAdmin.style.display = 'block';
+    } else if (component === 'perfil' && perfil) {
+        perfil.style.display = 'block';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    showComponent(handleButtonClick);
+});
