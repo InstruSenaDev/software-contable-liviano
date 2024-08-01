43f3c88e7f23dfcb2f3cc6449e6fd3cf5002698a
Laura Camila Vega <160734809+lauris-camila@users.noreply.github.com>
2024-07-31 19:56:55 -0500

    inicio de sesion listo

--- backend/controller.js
+++ backend/controller.js
@@ -5,67 +5,102 @@ const pool = require('./db.js')
 const saludo = (req, res) => {
     res.send("<h1>hola</h1>");
 }
-const usuarios =(req, res) => {
-    pool.query('SELECT * FROM usuarios',(error, results) =>
-    {
-        if (error){
+const usuarios = (req, res) => {
+    pool.query('SELECT * FROM usuarios', (error, results) => {
+        if (error) {
             console.error('Error al ejecutar la consulta:', error);
-            res.status(500).json({ error: 'Error interno del servidor'});
+            res.status(500).json({ error: 'Error interno del servidor' });
             return;
         }
         res.status(200).json(results.rows);
     });
 };
 
-const roles =(req, res) => {
-    pool.query('SELECT * FROM roles',(error, results) =>
-    {
-        if (error){
-            console.error('Error al ejecutar la consulta:',error);
-            res.status(500).json({error:'Error interno del servidor'});
+const roles = (req, res) => {
+    pool.query('SELECT * FROM roles', (error, results) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            res.status(500).json({ error: 'Error interno del servidor' });
             return;
         }
         res.status(200).json(results.rows);
     });
-};   
+};
 
 
 const registerUser = async (req, res) => {
     const { first_name, last_name, email, password } = req.body;
     try {
+        console.log('aaaaaaaaaaaaaaaa ', email);
         const hashedPassword = await bcrypt.hash(password, 10);
-        
         await pool.query(
             'INSERT INTO usuarios (nombre, apellido, correo, estado, contrasena,idrol) VALUES ($1, $2, $3, $4, $5,$6)',
-            [first_name, last_name, email, 'activo', hashedPassword,'1']
+            [first_name, last_name, email, 'activo', hashedPassword, '1']
         );
 
         res.status(201).json({ success: true, message: 'Usuario registrado exitosamente' });
     } catch (error) {
         console.error('Error al registrar usuario:', error);
-        res.status(500).json({ success: false, message: 'Error interno del servidor'+error });
+        res.status(500).json({ success: false, message: 'Error interno del servidor' + error });
     }
 };
 
 const inicioUser = async (req, res) => {
     const { email, password } = req.body;
+
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        
-        await pool.query(
-            'SELECT * FROM usuarios WHERE correo = $1 AND contrasena = $2', [email, password]
+        console.log('Password provided: ', password);
+        const result = await pool.query(
+            "SELECT * FROM usuarios WHERE correo = $1",
+            [email]
         );
 
-        res.status(201).json({ success: true, message: 'Usuario registrado exitosamente' });
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+            // const isMatch = await bcrypt.compare(password, user.contrasena);
+
+            const storedHash = user.contrasena.trim();
+            console.log('Stored hashed password: ', storedHash);
+            
+            const isMatch = await bcrypt.compare(password, storedHash);
+
+            if (isMatch) {
+              res
+                .status(200)
+                .json({ message: "Inicio de sesión exitoso", user: user });
+            } else {
+              res.status(400).json({ message: "Contraseña incorrecta" });
+            }
+      
+
+
+            // bcrypt.compare(password, storedHash, (err, result) => {
+            //     if (err) throw err;
+            //     console.log('Password match statusss:', result); // Debería ser true
+            //     if (result) {
+            //         res.status(200).json({ message: "Inicio de sesión exitoso", user: user.nombre });
+               
+            //     } else{
+            //         res.status(400).json({ message: "Contraseña incorrecta" });
+            //     }
+
+
+            // });
+            
+
+        } else {
+            res.status(404).json({ message: "Usuario no encontrado" });
+        }
     } catch (error) {
-        console.error('Error al registrar usuario:', error);
-        res.status(500).json({ success: false, message: 'Error interno del servidor'+error });
+        console.log("Error al iniciar sesión: ", error);
+        res.status(500).json({ message: "Error interno al procesar la solicitud", error });
     }
 };
 
+
 module.exports = {
-    saludo, 
-    usuarios, 
+    saludo,
+    usuarios,
     roles,
     registerUser,
     inicioUser
