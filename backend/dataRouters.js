ef9e197ec6baa452f6990ab8a5b3c4001c9d566b
ingrid-arce <160282857+ingrid-arce@users.noreply.github.com>
2024-08-02 09:14:49 -0500

    .

--- backend/dataRouters.js
+++ backend/dataRouters.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const { saludo, usuarios, roles, registerUser, inicioUser, registerProviders } = require('./controller'); // Asegúrate de que la ruta del controlador sea correcta
 
-router.get("/saludo", controller.saludo);
-router.get("/usuarios", controller.usuarios);
-router.get("/roles", controller.roles);
-router.post("/register",controller.registerUser);
-router.post("/login",controller.inicioUser);
+router.get("/saludo", saludo);
+router.get("/usuarios", usuarios);
+router.get("/roles", roles);
+router.post("/register", registerUser);
+router.post("/login",inicioUser);
 
 module.exports = router;
