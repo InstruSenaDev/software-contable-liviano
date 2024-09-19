96806176f3e647db9a761d6f52ef25f61408d972
ingrid-arce <ajulieth918@gmail.com>
2024-09-19 11:20:22 -0500

    revision pdf

--- Frontend/src/components/MiComponentePDF.jsx
+++ Frontend/src/components/MiComponentePDF.jsx
@@ -4,13 +4,23 @@ import PDF from './PDF';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 
 const MiComponentePDF = () => {
+
+  const estilobtn ={
+    backgroundColor: 'blue', // Color de fondo verde
+    color: 'white',              // Texto en blanco
+    padding: '10px 20px',        // Espaciado interno
+    border: 'none',              // Sin borde
+    borderRadius: '5px',         // Bordes redondeados
+    cursor: 'pointer',        
+  };
+
   return (
     <PDFDownloadLink document={<PDF />} fileName="informeCompra">
       {({ loading }) =>
         loading ? (
           <button>Cargando...</button>
         ) : (
-          <button>Descargar</button>
+          <button style={estilobtn}>Descargar</button>
         )
       }
     </PDFDownloadLink>
--- Frontend/src/components/PDF.jsx
+++ Frontend/src/components/PDF.jsx
@@ -1,8 +1,8 @@
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 
 const styles = StyleSheet.create({
-  page: { flexDirection: 'row', backgroundColor: '#E4E4E4' },
-  section: { margin: 10, padding: 10, flexGrow: 1 }
+  page: { flexDirection: "row", backgroundColor: "#E4E4E4" },
+  section: { margin: 10, padding: 10, flexGrow: 1 },
 });
 
 const PDF = () => (
@@ -12,7 +12,20 @@ const PDF = () => (
         <Text>Informe de Compra</Text>
       </View>
       <View style={styles.section}>
-        <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid ipsam sequi nihil quo laudantium doloremque velit temporibus consectetur veritatis soluta blanditiis expedita, tempora commodi quas odio voluptatem saepe autem distinctio sit? Qui earum porro quia eos placeat minima expedita, dignissimos repudiandae sapiente nisi dolorum officia dolor possimus accusamus. Magni neque eos perspiciatis impedit recusandae assumenda provident. Neque explicabo nesciunt earum fuga consequuntur eos amet perferendis deserunt voluptatem reiciendis atque aut, quas similique esse culpa vitae rem laboriosam, quo sint! Dicta dolore consequatur quam quod maiores. Aliquam, expedita iure? Soluta aspernatur voluptas, aliquid exercitationem repellat maiores dignissimos neque cumque saepe magnam.</Text>
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+          ipsam sequi nihil quo laudantium doloremque velit temporibus
+          consectetur veritatis soluta blanditiis expedita, tempora commodi quas
+          odio voluptatem saepe autem distinctio sit? Qui earum porro quia eos
+          placeat minima expedita, dignissimos repudiandae sapiente nisi dolorum
+          officia dolor possimus accusamus. Magni neque eos perspiciatis impedit
+          recusandae assumenda provident. Neque explicabo nesciunt earum fuga
+          consequuntur eos amet perferendis deserunt voluptatem reiciendis atque
+          aut, quas similique esse culpa vitae rem laboriosam, quo sint! Dicta
+          dolore consequatur quam quod maiores. Aliquam, expedita iure? Soluta
+          aspernatur voluptas, aliquid exercitationem repellat maiores
+          dignissimos neque cumque saepe magnam.
+        </Text>
       </View>
     </Page>
   </Document>
