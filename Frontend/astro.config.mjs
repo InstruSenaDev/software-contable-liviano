3025d345a26dc4da283be84eaa3aecfaf30f2c2d
LuisIbarguen <160256045+LuisIbarguen@users.noreply.github.com>
2024-09-19 15:22:52 -0500

    proyecto subido en vercel
    además se hicieron cambios de las imagenes de alguna paginas

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import "./src/styles/global.css"
+import "./src/styles/global.css";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-
-
   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
