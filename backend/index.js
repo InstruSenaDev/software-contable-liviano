b7ae78e725c14090ecf3edab657b6df835d7e068
LuisIbarguen <160256045+LuisIbarguen@users.noreply.github.com>
2024-09-20 09:58:46 -0500

    despliegue de backend en vercel

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 8080; // Usa el puerto proporcionado por Render
 const bcrypt = require('bcryptjs');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://tu-frontend.vercel.app', // Reemplaza con la URL de tu frontend en Vercel
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Puedes restringir los métodos permitidos
+    credentials: true // Si necesitas compartir cookies o información de autenticación
+}));
 app.use('/', dataRouters);
 
 app.listen(PORT, () => {
