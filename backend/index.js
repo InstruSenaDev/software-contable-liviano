cb4bc5528586aebf1f53dc479c3de37bfa85d83d
Laura Camila Vega <160734809+lauris-camila@users.noreply.github.com>
2024-09-20 10:14:41 -0500

    arreglo de backend

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,7 @@ const PORT = process.env.PORT || 8080; // Usa el puerto proporcionado por Render
 const bcrypt = require('bcryptjs');
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://tu-frontend.vercel.app', // Reemplaza con la URL de tu frontend en Vercel
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Puedes restringir los métodos permitidos
-    credentials: true // Si necesitas compartir cookies o información de autenticación
-}));
+app.use(cors());
 app.use('/', dataRouters);
 
 app.listen(PORT, () => {
