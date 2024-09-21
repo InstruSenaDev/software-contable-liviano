5413a808a0164496aa515cd8cf1fcb1abec740f5
LuisIbarguen <160256045+LuisIbarguen@users.noreply.github.com>
2024-09-21 07:51:18 -0500

    arreglo del informe
    ya volvió a cargar la interfaz

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -1,18 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import "./src/styles/global.css";
-import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-
+import './src/styles/global.css';
 
 export default defineConfig({
-
   site: 'https://lexingtonthemes.com',
   integrations: [tailwind(), sitemap(), react()],
-  plugins: [react()],
   output: 'server',
   adapter: vercel(),
-
 });
